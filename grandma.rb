94d5e7fa653a0d8b8f08d8131a4c34f3b970abd1
korey ko <koreykendrick1@gmail.com>
2020-05-27 18:03:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,9 @@
 def speak_to_grandma(phrase)
 	if phrase =  "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
-     elsif string != sring.upcase
+     elsif string != string.upcase
      "HUH?! SPEAK UP, SONNY!"
+   else"NO, NOT SINCE 1938!
     end
 end
 
