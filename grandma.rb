cfe5f622b95aac8ae3cf647f8dd4312d62312bbb
korey ko <koreykendrick1@gmail.com>
2020-05-27 18:03:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma(phrase)
      "I LOVE YOU TOO PUMPKIN!"
      elsif string != string.upcase
      "HUH?! SPEAK UP, SONNY!"
-   else"NO, NOT SINCE 1938!
+   else
+   "NO, NOT SINCE 1938!
     end
 end
 
